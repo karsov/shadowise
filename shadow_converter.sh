b9bd541e9ee582905e4110f16f9c3574a40a7a2b
Kalin Arsov <kalin.arsov@ft.com>
2021-01-26 17:36:42 +0200

    Allow swapping of primary and candidate destinations

--- shadow_converter.sh
+++ shadow_converter.sh
@@ -1,7 +1,24 @@
 #!/bin/bash -e
 
+primary="orig"
+candidate="shadow"
+
+for i in "$@"
+do
+case $i in
+	--promote-shadow)
+	primary="shadow"
+	candidate="orig"
+	shift # past argument with no value
+	;;
+	*)
+		# unknown option
+	;;
+esac
+done
+
 if [ $# -ne 1 ]; then
-	echo 'Usage: ./shadow_converter.sh <tag to compare with>'
+	echo 'Usage: ./shadow_converter.sh [--promote-shadow] <tag to compare with>'
 	exit
 fi
 
@@ -11,6 +28,10 @@ cd helm/*
 
 sed -E "s/({{ *.Values.service.name *}})/\1-shadow/" templates/deployment.yaml > templates/deployment-shadow.yaml
 sed -E "s/({{ *.Values.service.name *}})/\1-shadow/" templates/service.yaml > templates/service-shadow.yaml
+
+sed -i '' -E "s/%primary%/$primary/" templates/deployment-diferencia.yaml
+sed -i '' -E "s/%candidate%/$candidate/" templates/deployment-diferencia.yaml
+
 sed -i '' -E "s/({{ *.Values.service.name *}})/\1-orig/" templates/service.yaml
 sed -i '' -E "s/(image: +.+):{{ *.Chart.Version *}}/\1:$tag/" templates/deployment.yaml
 sed -i '' -E "s/({{ *.Values.service.name *}})/\1-orig/" templates/deployment.yaml
